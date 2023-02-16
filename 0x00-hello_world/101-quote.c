78cbedd0b0760264d1232b15414d5434d7784353
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-02-16 15:12:19 -0800

    add bouns

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define RET 1
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int i = 0;
+for (i = 0; i < (int)sizeof(s) - 1; i++)
+{
+putchar (s[i]);
+}
+
+return (RET);
+}
