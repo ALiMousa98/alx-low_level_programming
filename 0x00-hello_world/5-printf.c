09450392d434f694f7a182f2f4a801c9db4bb9a6
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-02-16 14:54:22 -0800

    add 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define RET 0
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (RET);
+}
