0813e14a1e625c302624c9ca0c4f8cc6f7784493
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-02-16 14:38:52 -0800

    add 4-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,12 @@
 
 #define RET 0
 
-int main()
+int main(void)
 {
 
+	/*
+	 * program to print a string using put function
+	 * */
 	puts("\"Programming is like building a multilingual puzzle");
-	return RET;
+	return( RET);
 }
