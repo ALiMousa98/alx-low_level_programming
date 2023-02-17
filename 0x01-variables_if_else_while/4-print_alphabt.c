adcdce2cef753fcc9ec09cf13056da5af9733ca1
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-02-17 15:30:40 -0800

    add 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+		{
+			c++;
+			continue;
+		}
+		
+		putchar(c++);
+	}
+	putchar('\n');
+
+	return (0);
+}
