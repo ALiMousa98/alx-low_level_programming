252a936f743dd5ebe8cc7c849e7b7a7a287a7929
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-02-27 11:19:37 -0800

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ *  _puts - length of the strin
+ *  @str: pointer to string
+ * Return: void
+*/
+
+void _puts(char *str)
+{
+
+	while (s[c] != '\0')
+		_putchar(s[c++]);
+	_putchar('\n');
+}
