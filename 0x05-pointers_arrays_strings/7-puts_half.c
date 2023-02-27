d08dd6a5994508d7149bcacb251e49b141010d6b
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-02-27 14:58:37 -0800

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,9 +16,9 @@ void puts_half(char *str)
 	if (c % 2 != 0)
 		c = (c - 1) / 2;
 	else
-		c = c / 2;
+		c = (c / 2) - 1;
 
-	while (str[c + 1] != '\0')
+	while (str[c] != '\0')
 		_putchar(str[c++]);
 	_putchar('\n');
 }
