18f40a3dc9e14cd23a93a8fa0c4c9806aa7bc5ec
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-02-27 14:46:36 -0800

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,12 @@ void puts_half(char *str)
 
 	int c = strlen(str);
 
-	if (c % 2 != 0)
-		c = (c-1) / 2;
+/*	if (c % 2 != 0)
+		c = (c - 1) / 2;
 	else
 		c = c / 2;
-
+*/
+	c = (c - 1) / 2;
 	while (str[c] != '\0')
 		_putchar(str[c++]);
 	_putchar('\n');
