b675a7dbf3a226c21d5b854d637eeebd898f999d
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-02 00:19:40 -0800

    0-strcat

---
+++ 0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - Concat 2 strings
+ *
+ * @dest: the array to concat into
+ * @src: the array to be concatinated
+ *
+ * Return: address of dest
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+	int i = strlen(dest) - 1;
+	int j = 0;
+
+	while (src[j] != '\0')
+	{
+		dest[i++] = src[j++];
+	}
+
+	return (dest);
+
+}
+
+
+
+
+
