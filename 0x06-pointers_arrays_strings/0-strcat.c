34de25df495f05db3a12e7f91bda83e7cf3233bd
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-02 01:13:17 -0800

    0-strcat 2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+
 
 /**
  * _strcat - Concat 2 strings
@@ -13,9 +13,11 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = strlen(dest) - 1;
+	int i = 0;
 	int j = 0;
 
+	while (dest[i] != '\0')
+		i++;
 	while (src[j] != '\0')
 	{
 		dest[i++] = src[j++];
