ff49786800493c8524f93153dda8d7aa4d1241a0
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-02 01:53:38 -0800

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncpy -  copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
+ * @n: number of chars to be copied
+ * Description: Copy the string pointed to by pointer `src` to
+ * the buffer pointed to by `dest`
+ * Return: Pointer to `dest`
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = -1;
+	int c = 0;
+
+	do {
+		index++;
+		dest[index] = src[index];
+		c++;
+	} while (src[index] != '\0' && c < n);
+
+	return (dest);
+}
