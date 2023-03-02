a63b24ec482b6cb68a216f8c23c4a6f9cfe93617
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-02 07:28:05 -0800

    7-1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,21 +9,33 @@
 
 char *leet(char *str)
 {
-	int i = 0;
+	/*
+	*int i = 0;
+	*
+	*while (str[i] != '\0')
+	*{
+	*	if (str[i] == 'a' || str[i] == 'A')
+	*		str[i] = '4';
+	*	else if (str[i] == 'e' || str[i] == 'E')
+	*		str[i] = '3';
+	*	else if (str[i] == 'o' || str[i] == 'O')
+	*		str[i] = '0';
+	*	else if (str[i] == 't' || str[i] == 'T')
+	*		str[i] = '7';
+	*	else if (str[i] == 'l' || str[i] == 'L')
+	*		str[i] = '1';
+	*	i++;
+	*}
+	*return (str);
+	*/
+	int i, j;
+	char *s1 = "aAeEoOtTlL";
+	char *s2 = "4433007711";
 
-	while (str[i] != '\0')
-	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
-		i++;
-	}
+	for (i = 0; str[i] != '\0'; i++)
+		for (j = 0; j < 10; j++)
+			if (str[i] == s1[j])
+				str[i] = s2[j];
 	return (str);
+
 }
