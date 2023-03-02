0d05b396293d355c636719d5927840f84afb4c28
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-02 02:46:05 -0800

    2-strncpy another way

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,19 +12,28 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = -1;
-	int c = 0;
+	/*
+	*int index = -1;
+	*int c = 0;
+	*
+	*do {
+	*	index++;
+	*	dest[index] = src[index];
+	*	c++;
+	*} while (src[index] != '\0' && c < n);
+	*while (c < n)
+	*{
+	*	dest[++index] = '\0';
+	*	c++;
+	*}
+	*/
 
-	do {
-		index++;
-		dest[index] = src[index];
-		c++;
-	} while (src[index] != '\0' && c < n);
-	while (c < n)
-	{
-		dest[++index] = '\0';
-		c++;
-	}
+	int x = 0;
+
+	for ( ; x < n && src[x] != '\0'; x++)
+		dest[x] = src[x];
+	for ( ; x < n; x++)
+		dest[x] = '\0';
 
 	return (dest);
 }
