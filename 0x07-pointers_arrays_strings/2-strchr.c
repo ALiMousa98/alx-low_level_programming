5b962c890c457668b0394e8849d02e9b1183f49f
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-06 15:21:51 -0800

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,21 +15,11 @@ char *_strchr(char *s, char c)
 
 	while (s[i] != '\0')
 	{
-		/*
-		 * if (s[i] != c)
-		 *	i++;
-		 *else
-		 *	break;
-		 */
-		if (s[i] == c)
-		{
-			s += i;
-			return (s);
-		}
+		
+		 if (s[i] != c)
+			i++;
+		else
+			break;
 	}
-	/*
-	 * if (i > 0)
-	 *	return (NULL);
-	 */
 	return ('\0');
 }
