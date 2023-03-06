11444f05688546cf0768637118a377bc2d4001e7
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-06 07:44:42 -0800

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - copies n bytes from memory area src to memory area dest
+ *
+ * @s: array to be searched
+ * @c: the character to search for
+ * Return: pointer the array after get the first occurance
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+			i++;
+		else
+			break;
+	}
+	return (s + i);
+}
