83241d99ef39752e862af77487db08018c28ec04
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-06 16:31:10 -0800

    4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strpbrk - locate character in string
+  * @s: source string
+  * @accept: character to find
+  *
+  * Return: the string from character found
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return ('\0');
+}
