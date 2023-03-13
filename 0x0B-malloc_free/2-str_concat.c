e73a65e8ca535e0c526104a3812378445d63846e
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-13 15:16:07 -0700

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * str_concat  - returns a pointer to a newly allocated space in memory,
+ * which contains a new concatenated array.
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to the new allocated array
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *dest = malloc(strlen(s1) + strlen(s2) + 1);
+
+	if (dest == NULL)
+		return (NULL);
+	strcat(dest, s1);
+	strcat(dest, s2);
+
+	return (dest);
+}
