742bae47c6d1938ffec1e4e731992e3b99850fe6
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-13 15:56:43 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,10 @@ char *str_concat(char *s1, char *s2)
 	char *dest;
 
 	if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	dest = malloc(strlen(s1) + strlen(s2) + 1);
-
 	if (dest == NULL)
 		return (NULL);
 	strcat(dest, s1);
