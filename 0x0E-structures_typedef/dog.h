9a8dc2ade8750d54d4703a0174000c9a7bcc2d8f
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-03-20 15:53:46 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog structure
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+}
+
+dog_t;
+
+#endif
