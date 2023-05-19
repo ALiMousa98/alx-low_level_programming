bbf2d61fab838a7c8883f47612e8555018b7f703
AliMousa98 <ali.abdelaal1998@gmail.com>
2023-05-18 17:52:14 -0700

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,34 +10,32 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
-    dlistint_t *last_node;
+	dlistint_t *new_node, *last_node;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
-    if (*head == NULL) {
-        // If the list is empty, the new node becomes the head
-        new_node->prev = NULL;
-        *head = new_node;
-    } else {
-        // Traverse to the last node
-        last_node = *head;
-        while (last_node->next != NULL) {
-            last_node = last_node->next;
-        }
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		last_node = *head;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
 
-        // Link the new node to the last node
-        last_node->next = new_node;
-        new_node->prev = last_node;
-    }
+		last_node->next = new_node;
+		new_node->prev = last_node;
+	}
 
-    return (new_node);
+	return (new_node);
 }
